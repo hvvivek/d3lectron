163728a63b2e24ee8ff4e9fce3e03c87d87db656
hvvivek <hvvivek2007@gmail.com>
2018-11-14 21:49:28 -0500

    Added Data connection to ellipse

--- extensions/Ellipse/ellipse.js
+++ extensions/Ellipse/ellipse.js
@@ -10,6 +10,14 @@ class Ellipse
     {
         console.log(config_container)
         var config = {}
+        config.dataConnect = $(config_container).find("input[name=connect_data]")[0].checked
+        if(config.dataConnect)
+        {
+            config.dataSource = $(config_container).find("input[name=data]").val()
+            console.log(config.dataSource)
+            config.dataSource = workspace.data_sources[config.dataSource]
+            console.log(config.dataSource)
+        }
         config.rx = $(config_container).find("input[name=rx]").val()
         config.ry = $(config_container).find("input[name=ry]").val()
         config.cx = $(config_container).find("input[name=cx]").val()
@@ -23,10 +31,12 @@ class Ellipse
             config.optionalSettings.push({key: prop.attr("name"), value: prop.val()})
         }
         config.parent = getActiveLayer()
-        var id = randomString(5)
-        config.id = id
-        config.parent.addTemplate(id, new Ellipse(config))
-        config.parent.getTemplate(id).compile()
+
+        // var id = randomString(5)
+        // config.id = $(config_container).find("input[name=id]").val()
+        config.canvas = getActiveLayer().getTemplate(workspace.activeTemplateId).getSVGGroup()
+
+        config.parent.getTemplate(workspace.activeTemplateId).compile(Ellipse, config)
     }
 
     setConfig(config)
@@ -37,34 +47,27 @@ class Ellipse
         this.cx = config.cx
         this.cy = config.cy
         this.parent = config.parent
+        this.canvas = config.canvas
+        this.width = 1920
+        this.height = 1080
     }
 
     compile()
     {
-        var canvas = d3.select("#" + this.parent.getSVGGroup())
+        var canvas = d3.select("#" + this.canvas)
 
         if(!this.canvas_group)
         {
             var random_string = randomString(20)
-            canvas.append("g").attr("class", random_string)
+            canvas.append("g").attr("id", random_string)
             this.canvas_group = random_string
 
         }
         
-        canvas = canvas.select("." + this.canvas_group)
-        // var xFeature = this.xFeature
-        // var yFeature = this.yFeature
-
-       
-        // this.xScale = d3.scaleLinear()
-        //                 .domain(getDomain(xFeature))
-        //                 .range([this.left, this.width - this.right])
-
-        // this.yScale = d3.scaleLinear()
-        //                 .domain(getDomain(yFeature))
-        //                 .range([this.height - this.bottom, this.top])
-                
-        // console.log(canvas)
+        canvas = canvas.select("#" + this.canvas_group)
+        console.log("#" + this.canvas_group)
+        if(!this.config.dataConnect)
+        {
         canvas
             // .data(this.dataSource.data)
             // .enter()
@@ -73,7 +76,45 @@ class Ellipse
                 .attr("ry",      this.getRyFunction())
                 .attr("cx",     this.getCxFunction())
                 .attr("cy",     this.getCyFunction())
-                    
+        }
+        else
+        {
+            console.log("Here")
+            var xFeature = this.config.cx
+            var yFeature = this.config.cy
+
+        
+            this.xScale = d3.scaleLinear()
+                            .domain(this.getDomain(xFeature))
+                            .range([0, this.width])
+
+            this.yScale = d3.scaleLinear()
+                            .domain(this.getDomain(yFeature))
+                            .range([this.height, 0])
+                            
+            var plot = canvas
+                .selectAll("ellipse")
+                .data(this.config.dataSource.data)
+
+            plot
+                .enter()
+                    .append("ellipse")
+                    .attr("rx",      this.getRxFunction())
+                    .attr("ry",      this.getRyFunction())
+                    .attr("cx",     this.getCxFunction())
+                    .attr("cy",     this.getCyFunction())
+            
+            plot
+                .attr("rx",      this.getRxFunction())
+                .attr("ry",      this.getRyFunction())
+                .attr("cx",     this.getCxFunction())
+                .attr("cy",     this.getCyFunction())
+
+            plot.exit().transition()
+            .style("opacity", 0).remove()
+
+            
+        }
         // canvas
         //     .attr("r",      this.getRadiusFunction())
         //     .attr("cx",     this.getCxFunction())
@@ -110,14 +151,35 @@ class Ellipse
         return this.ry
     }
 
+
     getCxFunction()
     {
-        return this.cx
+        var xScale = this.xScale
+        if(this.config.dataConnect && !isValidLength(this.config.cx))
+        {
+            var feature = this.config.cx
+            return function(d){ return xScale(d[feature])}
+        }
+        else
+        {
+            return this.config.cx
+        }
+        // return this.cx
     }
 
     getCyFunction()
     {
-        return this.parent.getHeight() - this.cy
+        var yScale = this.yScale
+        if(this.config.dataConnect && !isValidLength(this.config.cy))
+        {
+            var feature = this.config.cy
+            return function(d){ return yScale(d[feature])}
+        }
+        else
+        {
+            return this.config.cy
+        }
+        // return this.parent.getHeight() - this.cy
     }
 
     // getFillFunction()
@@ -129,12 +191,9 @@ class Ellipse
     //Utility functions
     getDomain(feature)
     {
-        var data = this.dataSource.data
+        var data = this.config.dataSource.data
         var min = d3.min(data, function(d){return parseInt(d[feature])})
         var max = d3.max(data, function(d){return parseInt(d[feature])})
         return [min, max]
     }
 }
-
-
-
